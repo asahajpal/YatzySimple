b662243a03c1c19bb9ba924856253d7e8b9fe673
Anurag Sahajpal <Anurag.Sahajpal@stralfors.no>
2025-03-19 22:00:55 +0100

    added unit test for validating total score and sum of categories scores always match

--- YatzyTests/GameContextTests.cs
+++ YatzyTests/GameContextTests.cs
@@ -1,19 +1,16 @@
-using System.Collections.Generic;
-using Xunit;
-using Moq;
 using YatzySimple.Core;
 using YatzySimple.States;
 using YatzySimple.Interfaces;
+using Moq;
 
 namespace YatzyTests
 {
-public class GameContextTests
+    public class GameContextTests
     {
         [Fact]
         public void PlayNextTurn_ShouldReachGameOverState_WhenAllCategoriesAreScored()
         {
             // Arrange
-
             var gameContext = new GameContext();
             
             // Set all categories to non-zero scores
@@ -36,7 +33,6 @@ public class GameContextTests
         public void PlayNextTurn_ShouldTransitionToScoringState_AfterRollingDice()
         {
             // Arrange
-          
             var gameContext = new GameContext();
 
             // Act
@@ -53,12 +49,11 @@ public class GameContextTests
             var mockPlayer = new Mock<IPlayer>();
             mockPlayer.Setup(p => p.Dice).Returns(new int[] { 1, 1, 1, 1, 1 });
             mockPlayer.Setup(p => p.RollDice());
-           
             mockPlayer.Setup(p => p.ScoreDice(It.IsAny<GameContext>())).Callback<GameContext>(context =>
             {
                 context.UpdateScore("Ones", 5);
             });
-            
+
             var gameContext = new GameContext
             {
                 Player = mockPlayer.Object
@@ -70,6 +65,27 @@ public class GameContextTests
 
             // Assert
             Assert.Equal(5, gameContext.GetScores()["Ones"]);
+        }
+
+        [Fact]
+        public void TotalScore_ShouldBeSumOfIndividualCategoryScores()
+        {
+            // Arrange
+            var gameContext = new GameContext();
+
+            // Update scores for different categories
+            gameContext.UpdateScore("Ones", 1);
+            gameContext.UpdateScore("Twos", 2);
+            gameContext.UpdateScore("Threes", 3);
+            gameContext.UpdateScore("Fours", 4);
+            gameContext.UpdateScore("Fives", 5);
+            gameContext.UpdateScore("Sixes", 6);
+
+            // Act
+            int totalScore = gameContext.CalculateTotalScore();
+
+            // Assert
+            Assert.Equal(21, totalScore); // 1 + 2 + 3 + 4 + 5 + 6 = 21
         }
     }
 }
